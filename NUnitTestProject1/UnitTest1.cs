c5bfdd7c232356d898fef3e29495aa11331ff245
“shrawanamit” <amitkumar06111@gmail.com>
2020-06-15 17:08:31 +0530

    welcome to cab invoice generator

---
+++ NUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using ConsoleApp2;
+
+namespace NUnitTestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
